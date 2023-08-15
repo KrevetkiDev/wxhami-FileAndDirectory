a642c8d64813e2c4883416432ac1575502913b71
wxhami <d18102002@mail.ru>
2023-08-15 14:15:31 +0300

    add Spam method

--- src/FilesAndDirectories/FilesAndDirectories/Program.cs
+++ src/FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -1,24 +1,27 @@
-﻿bool playerChoice;
-
-while (playerChoice = true)
+﻿void Spam()
 {
-    Console.WriteLine("Choose action: 1-create directories, 2- delete all");
-    int playerChoiceAction = Convert.ToInt32(Console.ReadLine());
-    if (playerChoiceAction == 1)
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            Directory.CreateDirectory(@"C:\Users\d1810\Рабочий стол\sorry, it's a prank" + i);
-        }
-    }
-    else
-    {
-        DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\d1810\Рабочий стол\");
-        var directories = directoryInfo.GetDirectories("sorry, it's a prank*");
+    bool playerChoice;
 
-        foreach (var directory in directories)
+    while (playerChoice = true)
+    {
+        Console.WriteLine("Choose action: 1-create directories, 2- delete all");
+        int playerChoiceAction = Convert.ToInt32(Console.ReadLine());
+        if (playerChoiceAction == 1)
         {
-            directory.Delete();
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.CreateDirectory(@"C:\Users\d1810\Рабочий стол\sorry, it's a prank" + i);
+            }
+        }
+        else
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\d1810\Рабочий стол\");
+            var directories = directoryInfo.GetDirectories("sorry, it's a prank*");
+
+            foreach (var directory in directories)
+            {
+                directory.Delete();
+            }
         }
     }
 }
