6128d4bce0e10285f2228fb238467f436b88c3c8
wxhami <d18102002@mail.ru>
2023-08-15 14:03:49 +0300

    task done

--- src/FilesAndDirectories/FilesAndDirectories/Program.cs
+++ src/FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -3,13 +3,22 @@
 while (playerChoice = true)
 {
     Console.WriteLine("Choose action: 1-create directories, 2- delete all");
-    int  playerChoiceAction = Convert.ToInt32(Console.ReadLine());
+    int playerChoiceAction = Convert.ToInt32(Console.ReadLine());
     if (playerChoiceAction == 1)
     {
-        
+        for (int i = 0; i < 100; i++)
+        {
+            Directory.CreateDirectory(@"C:\Users\d1810\Рабочий стол\sorry, it's a prank" + i);
+        }
     }
     else
     {
-        
+        DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\d1810\Рабочий стол\");
+        var directories = directoryInfo.GetDirectories("sorry, it's a prank*");
+
+        foreach (var directory in directories)
+        {
+            directory.Delete();
+        }
     }
 }
