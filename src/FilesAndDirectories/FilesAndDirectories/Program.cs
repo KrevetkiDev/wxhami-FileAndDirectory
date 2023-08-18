92e7df05c956c42b7b9fa57488b32609f626574c
wxhami <d18102002@mail.ru>
2023-08-18 16:42:46 +0300

    fix Process method + add check IndexOutOfRangeException, DirectoryNotFoundException, IOException

--- src/FilesAndDirectories/FilesAndDirectories/Program.cs
+++ src/FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.CompilerServices;
+using FilesAndDirectories;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Specify the path to the directory");
+string Path = Console.ReadLine();
+
+try
+{
+    Process(Path);
+}
+
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("There are no files in the directory");
+    throw;
+}
+
+catch (DirectoryNotFoundException)
+{
+    Console.WriteLine("Directory not found");
+    throw;
+}
+
+catch (IOException)
+{
+    Console.WriteLine("Invalid directory name");
+    throw;
+}
+
+
+void Process(string path)
+{
+    path = Path;
+    DirectoryInfo directoryInfo = new DirectoryInfo(path);
+    var allFiles = directoryInfo.GetFiles("*.txt");
+    allFiles.Sort();
+    string filePath = allFiles[0].FullName;
+    var text = File.ReadAllText(filePath);
+    Console.WriteLine(text);
+    
+}
