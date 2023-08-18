d274ab78174102a08673b8e687a8b0e80d7048c1
wxhami <d18102002@mail.ru>
2023-08-18 14:49:53 +0300

    add ArrayExtensions class + Sort method

---
+++ src/FilesAndDirectories/FilesAndDirectories/ArrayExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace FilesAndDirectories;
+
+public static class ArrayExtensions
+{
+    public static void Sort(this FileInfo[] fileInfos)
+    {
+        for (int i = 0; i < fileInfos.Length - 1; i++)
+        {
+            for (int j = i + 1; j < fileInfos.Length; j++)
+            {
+                if (fileInfos[i].LastWriteTimeUtc > fileInfos[j].LastWriteTimeUtc)
+                {
+                    var temp = fileInfos[i];
+                    fileInfos[i] = fileInfos[j];
+                    fileInfos[j] = temp;
+                }
+            }
+        }
+    }
+}
