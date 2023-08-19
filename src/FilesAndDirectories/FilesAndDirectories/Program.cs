4e91721f63277506441a4942aa4c4292605f8067
wxhami <d18102002@mail.ru>
2023-08-19 15:48:49 +0300

    add censor method

--- src/FilesAndDirectories/FilesAndDirectories/Program.cs
+++ src/FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Censor();
 
-Console.WriteLine("Hello, World!");
+void Censor()
+{
+    List<string> StopWords = new List<string>()
+    {
+        "блят",
+        "еб",
+        "муда",
+        "ебан",
+        "ебучий",
+        "хуи",
+        "хуя",
+        "долб",
+        "хуй",
+        "муди",
+        "бляд"
+    };
+    Console.WriteLine("Specify the path to the directory");
+    string Path = Console.ReadLine();
+    string newWord = "***";
+    FileInfo fileInfo = new FileInfo(Path);
+    
+    Process(@"C:\Users\d1810\Рабочий стол\папка\случай в казино.txt");
+
+    void Process(string path)
+    {
+        path = Path;
+
+        string textForCensor = File.ReadAllText(path);
+        
+        
+        foreach (var word in StopWords)
+        {
+            
+           textForCensor = textForCensor.Replace(word, newWord, StringComparison.InvariantCultureIgnoreCase);
+           
+        }
+        File.WriteAllText(fileInfo.DirectoryName + "\\New.txt", textForCensor);
+    }
+}
