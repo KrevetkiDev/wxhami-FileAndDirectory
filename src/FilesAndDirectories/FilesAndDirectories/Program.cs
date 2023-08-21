56a4f08269c2c8178f3039b64437fb582fb0b202
wxhami <d18102002@mail.ru>
2023-08-21 11:06:57 +0300

    review fixies

--- src/FilesAndDirectories/FilesAndDirectories/Program.cs
+++ src/FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -2,24 +2,32 @@
 {
     while (true)
     {
-        Console.WriteLine("Choose action: 1-create directories, 2- delete all");
+        Console.WriteLine("Choose action: 1 - create directories, 2 - delete all");
         int playerChoiceAction = Convert.ToInt32(Console.ReadLine());
-        if (playerChoiceAction == 1)
+        switch (playerChoiceAction)
         {
-            for (int i = 0; i < 100; i++)
-            {
-                Directory.CreateDirectory(@"C:\Users\d1810\Рабочий стол\sorry, it's a prank" + i);
-            }
-        }
-        else
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\d1810\Рабочий стол\");
-            var directories = directoryInfo.GetDirectories("sorry, it's a prank*");
+            case 1:
+                for (int i = 0; i < 100; i++)
+                {
+                    Directory.CreateDirectory(@"C:\Users\d1810\Рабочий стол\sorry, it's a prank" + i);
+                }
 
-            foreach (var directory in directories)
-            {
-                directory.Delete();
-            }
+                break;
+
+            case 2:
+                DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\d1810\Рабочий стол\");
+                var directories = directoryInfo.GetDirectories("sorry, it's a prank*");
+
+                foreach (var directory in directories)
+                {
+                    directory.Delete();
+                }
+
+                break;
+
+            default:
+                Console.WriteLine("Incorrect comand");
+                break;
         }
     }
 }
